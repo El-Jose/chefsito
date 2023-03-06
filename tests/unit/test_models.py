36f995a975e2f4f2aa512a8d34949778b2cf0915
benja <benprimero@gmail.com>
2023-03-06 10:52:55 -0500

    se a organizado algunos archivos(__init__) y las pruebas inician correctamente

--- tests/unit/test_models.py
+++ tests/unit/test_models.py
@@ -1,8 +1,8 @@
-from models import User
+from app.login.models import User
 
 
 def test_new_user():
     try_user = User('bateman@gmail.com', 'TWisaN')
     assert try_user.email == 'bateman@gmail.com'
-    assert try_user.hashed_password != 'TWisaN'
+    assert try_user.password != 'TWisaN'
     assert try_user.role == 'user'
