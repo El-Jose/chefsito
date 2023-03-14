ed4914894b4adf5d82d8d437a6bd3fc6839c4428
benja <benprimero@gmail.com>
2023-03-14 16:19:19 -0500

    se cambia el nombre de la prueba, el tclient a test_client y se agrega un init_database a conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,30 @@
 import pytest
 
-from app import current_app
+from app import current_app, db
+from app.login.models import User
 
 
 @pytest.fixture(scope='module')
-def tclient():
-    with current_app.test_client() as test_client:
-        yield test_client
+def test_client():
+    with current_app.test_client() as client:
+        yield client
+
+
+@pytest.fixture(scope='module')
+def init_database(tclient):
+    # Create the database and the database table
+    db.create_all()
+
+    # Insert user data
+    user1 = User(email='userone@gmail.com', password_plaintext='contraseñauwu')
+    user2 = User(email='usertwo@gmail.com', password_plaintext='uwucontraseña')
+    db.session.add(user1)
+    db.session.add(user2)
+
+    # Commit the changes for the users
+    db.session.commit()
+
+    yield  # this is where the testing happens!
+
+    db.drop_all()
+
--- tests/functional/test_users.py
+++ tests/functional/test_users.py
@@ -1,11 +1,11 @@
 
-def test_my_register(tclient):
-    response = tclient.post('/register', json={'email': 'kazu@gmail.com', 'password': 'toradesu'})
+def test_register(test_client):
+    response = test_client.post('/register', json={'email': 'kazu@gmail.com', 'password': 'toradesu'})
     assert response.status_code == 200
     assert 'register successful' in response.get_data(as_text=True)
 
 
-def test_my_login(tclient):
-    response = tclient.post('/login', json={'email': 'kazu@gmail.com', 'password': 'toradesu'})
+def test_login_login(test_client):
+    response = test_client.post('/login', json={'email': 'kazu@gmail.com', 'password': 'toradesu'})
     assert response.status_code == 200
     assert '"correct password"\n' in response.get_data(as_text=True)   
