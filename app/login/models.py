54f19f7668a4293b63dc9812fb903ed20d1f8b22
benja <benprimero@gmail.com>
2023-03-22 13:00:49 -0500

    se agrego el view reset completamente funcional

--- app/login/models.py
+++ app/login/models.py
@@ -6,14 +6,16 @@ db = SQLAlchemy()
 
 @dataclass
 class User(db.Model):
-    __tablename__ = 'User'
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     email = db.Column(db.String(255))
     password = db.Column(db.String(255))
+    temp_num = db.Column(db.Integer)
 
     def __init__(self, email, password):
         self.email = email
         self.password = password
+
 
     def __repr__(self):
         return f"User {self.email}"
--- app/login/views.py
+++ app/login/views.py
@@ -1,12 +1,13 @@
 
-import random
+import secrets
 
-from flask import Blueprint, Flask, abort, current_app, jsonify, request
+from flask import Blueprint, Flask, abort, current_app, jsonify, request, redirect, url_for
 from werkzeug.security import check_password_hash, generate_password_hash
 
-from app.login.models import User, Users, db
+from app.login.models import User, db
 
 login = Blueprint('login', __name__)
+# ______________________________________________________________________________
 
 
 @login.route('/login', methods=['POST'])
@@ -21,6 +22,7 @@ def login_view():
         else:
             return abort(501, description="error trying to login")
     return jsonify('login successful!')
+# ______________________________________________________________________________
 
 
 @login.route('/register', methods=['POST'])
@@ -37,16 +39,44 @@ def register_view():
         except Exception as e:
             return abort(501, description=f"error trying to register: {e}")
         return jsonify('register successful')
+# ______________________________________________________________________________
 
 
 @login.route('/recovery', methods=['POST'])
-def pass_recovery():
+def password_recovery():
     if request.method == 'POST':
         email = request.json['email']
         user = User.query.filter_by(email=email).first()
         if user:
-            codigo = ''.join([str(random.randint(0, 9)) for _ in range(6)])
-            user.codigo_temporal = codigo
-            db.session.commit()
+            codigo = str(secrets.randbelow(1000000)).zfill(6)
+            user.temp_num = codigo
+            db.session.add(user)
+            db.session.commit()# aca no hace el commit a la bd
             return jsonify({'codigo': codigo})
     return jsonify({'mensaje': 'Correo electrónico no encontrado'})
+# ______________________________________________________________________________
+
+
+@login.route('/reset', methods=['POST'])
+def passord_reset():
+    import pdb; pdb.set_trace()
+    if request.method == 'POST':
+        data = request.json
+        email = data.get('email')
+        codigo = int(data.get('codigo'))
+        user = User.query.filter_by(email=email).first()
+        if user and user.temp_num == codigo:
+            pswrd = data.get('password')
+            confirm_password = data.get('confirm')
+            if pswrd == confirm_password:
+                # Actualizar la contraseña del usuario
+                pswrd1 = generate_password_hash(pswrd)
+                user.password = pswrd1
+                user.temp_num = None
+                db.session.commit()
+                return jsonify({'La contraseña se ha actualizado correctamente':'success'})
+            else:
+                return jsonify({'message': 'Las contraseñas no coinciden'}), 400
+        else:
+            return jsonify({'message': 'El correo electrónico o el código son incorrectos'}), 400
+# ______________________________________________________________________________
